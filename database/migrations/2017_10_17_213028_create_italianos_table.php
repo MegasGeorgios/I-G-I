62a54c0902bbe78d092255e95d90bb92c7e474f0
MegasGeorgios <georgaky.71@gmail.com>
2018-08-18 20:59:49 +0200

    update

--- database/migrations/2017_10_17_213028_create_italianos_table.php
+++ database/migrations/2017_10_17_213028_create_italianos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItaliansTable extends Migration
+class CreateItalianosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateItaliansTable extends Migration
      */
     public function up()
     {
-        Schema::create('italians', function (Blueprint $table) {
+        Schema::create('italianos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('palabra');
             $table->string('significado');
