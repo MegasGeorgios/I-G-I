d8066322b9b4fff9a1093267baebe46847d2734d
MegasGeorgios <georgaky.71@gmail.com>
2018-08-18 21:31:27 +0200

    update

--- resources/views/idiomas/create.blade.php
+++ resources/views/idiomas/create.blade.php
@@ -3,28 +3,32 @@
 @section('content')
 <form method="POST" class="was-validated" action="{{ route('idioma.store') }}">
 {{ csrf_field() }}
-  <div class="row" >
-    <div class="col-md-3 mb-3 center-block">
-      <label for="validationServer01">{{$idioma}}</label>
-      <input type="text" class="form-control is-valid" id="validationServer01" name="palabra" placeholder="Palabra"  required>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationServer02">español</label>
-      <input type="text" class="form-control is-valid" id="validationServer02" name="significado" placeholder="Significado"  required>
-    </div>
-    <div class="col-md-4 mb-3">
-      <label>Categoria</label>
-      <select name="id_categoria" class="custom-select d-block my-3"  required>
-        <option value="">Seleccione</option>
-        @foreach($nombre_categorias as $categoria)
-        <option value="{{$categoria->id}}">{{$categoria->nombre_categoria}}</option>
-        @endforeach
-      </select>
+  <div class="container center-block">
+    <div class="row" >
+      <div class="col-md-3 mb-3 ">
+        <label for="validationServer01">{{$idioma}}</label>
+        <input type="text" class="form-control is-valid" id="validationServer01" name="palabra" placeholder="Palabra"  required>
+      </div>
+      <div class="col-md-3 mb-3">
+        <label for="validationServer02">español</label>
+        <input type="text" class="form-control is-valid" id="validationServer02" name="significado" placeholder="Significado"  required>
+      </div>
+      <div class="col-md-3 mb-3">
+        <label>Categoria</label>
+        <select name="id_categoria" class="custom-select d-block" style="height: 36px;" required>
+          <option value="">Seleccione</option>
+          @foreach($nombre_categorias as $categoria)
+          <option value="{{$categoria->id}}">{{$categoria->nombre_categoria}}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="col-md-3 mb-3">
+        <label></label>
+          <button class="form-control "  type="submit">Guardar</button>
+          <input type="hidden" name="idioma" value="{{$idioma}}">
+      </div>
     </div>
   </div>
-  <br>
-  <input type="hidden" name="idioma" value="{{$idioma}}">
-  <button class="btn btn-primary center-block"  type="submit">Guardar</button>
   </form>
 <br><br><br>
 
