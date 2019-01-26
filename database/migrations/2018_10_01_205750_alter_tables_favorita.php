6f7e44807cb05a810730cae1d8a22614f6ee6dd6
megasgeorgios <georgaky.71@gmail.com>
2019-01-26 12:36:07 +0100

    alter tables

--- database/migrations/2018_10_01_205750_alter_tables_favorita.php
+++ database/migrations/2018_10_01_205750_alter_tables_favorita.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTablesSlug extends Migration
+class AlterTablesFavorita extends Migration
 {
     /**
      * Run the migrations.
