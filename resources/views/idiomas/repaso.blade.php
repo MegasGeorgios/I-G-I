7cf699ddb44dab84d335f232c92d6cef1a290932
Georgios <georgios.botas@corusconsulting.com>
2019-05-21 09:44:54 +0200

    posicion boton exportar pdf en repaso

--- resources/views/idiomas/repaso.blade.php
+++ resources/views/idiomas/repaso.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
 <div id="vue">
-  <repaso-componente idioma="{{ $idioma }}"></repaso-componente>
-  <form action="{{ url('/idioma/'.$idioma.'/pdf') }}" method="get" style="padding-top: 10px; padding-bottom: 10px;">
-    <button class="btn btn-primary center-block" style="width:150px;" type="submit">Exportar todas a PDF</button>
-  </form>
+	<form action="{{ url('/idioma/'.$idioma.'/pdf') }}" method="get" style="padding-top: 10px; padding-bottom: 10px;">
+    	<button class="btn btn-primary center-block" style="width:150px;" type="submit">Exportar todas a PDF</button>
+  	</form>
+  	
+  	<repaso-componente idioma="{{ $idioma }}"></repaso-componente>
 </div>
 @endsection
